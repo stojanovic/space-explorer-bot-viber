5679bd205ee0e3cb3c98d044a605e4d290fe84fe
simalexan <alexander.simovic@gmail.com>
2016-11-12 17:23:53 +0100

    expanding row size for the main menu buttons

--- bot.js
+++ bot.js
@@ -5,10 +5,10 @@ var botBuilder = require('claudia-bot-builder'),
 var mainMenu = () => {
   return vbTemplate.Text(`Hello, this is Space Explorer, your message was ${request.text}`)
     .addReplyKeyboard()
-    .addKeyboardButton('Astronomy Picture of the Day', 'reply', 'APOD', 3, 1)
-    .addKeyboardButton('Photos from Mars Rovers', 'reply', 'ROVERS', 3, 1)
-    .addKeyboardButton('Space Station Position', 'reply', 'ISS', 3, 1)
-    .addKeyboardButton('People in Space', 'reply', 'PEOPLE_IN_SPACE', 3, 1)
+    .addKeyboardButton('Astronomy Picture of the Day', 'reply', 'APOD', 3, 2)
+    .addKeyboardButton('Photos from Mars Rovers', 'reply', 'ROVERS', 3, 2)
+    .addKeyboardButton('Space Station Position', 'reply', 'ISS', 3, 2)
+    .addKeyboardButton('People in Space', 'reply', 'PEOPLE_IN_SPACE', 3, 2)
     .addKeyboardButton('About Bot', 'reply', 'ABOUT', 6, 1)
     .get();
 };
