582474a40fe7afa17e1c2cb9d00c40426acc09e6
simalexan <alexander.simovic@gmail.com>
2016-11-24 02:41:56 +0100

    updated images for start menu

--- bot/start-menu.js
+++ bot/start-menu.js
@@ -9,31 +9,31 @@ module.exports = function startMenu(text) {
         TextSize: 'large',
         BgColor: '#f6d95e',
         BgMediaType: 'picture',
-        BgMedia: 'https://github.com/stojanovic/space-explorer-bot-viber/raw/master/images/galaxy.jpeg'
+        BgMedia: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot-viber/master/images/galaxy.jpeg'
       })
       .addKeyboardButton(`<b>Photos from Rovers on Mars</b>`, 'Mars Rovers', 3, 2, {
         TextSize: 'large',
         BgColor: '#f6d95e',
         BgMediaType: 'picture',
-        BgMedia: 'https://github.com/stojanovic/space-explorer-bot-viber/raw/master/images/curiosity.jpg'
+        BgMedia: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot-viber/master/images/curiosity.jpg'
       })
       .addKeyboardButton(`<b>Where is the ISS now?</b>`, 'ISS', 3, 2, {
         TextSize: 'large',
         BgColor: '#f6d95e',
         BgMediaType: 'picture',
-        BgMedia: 'https://github.com/stojanovic/space-explorer-bot-viber/raw/master/images/iss.jpg'
+        BgMedia: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot-viber/master/images/iss.jpg'
       })
       .addKeyboardButton(`<b>How many people are in space right now?</b>`, 'People in Space', 3, 2, {
         TextSize: 'large',
         BgColor: '#f6d95e',
         BgMediaType: 'picture',
-        BgMedia: 'https://github.com/stojanovic/space-explorer-bot-viber/raw/master/images/space-people.jpg'
+        BgMedia: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot-viber/master/images/space-people.jpg'
       })
       .addKeyboardButton(`<b>What about me, your space bot?</b>`, 'About', 6, 1, {
         TextSize: 'large',
         BgColor: '#f6d95e',
         BgMediaType: 'picture',
-        BgMedia: 'https://github.com/stojanovic/space-explorer-bot-viber/raw/master/images/claudia-chatbot.png'
+        BgMedia: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot-viber/master/images/claudia-chatbot.png'
       })
     .get()
 }
