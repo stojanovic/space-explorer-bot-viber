30fc975376176c9c95201c891942f4a08b668125
simalexan <alexander.simovic@gmail.com>
2016-11-24 02:09:00 +0100

    adding missing rover details

--- bot/curiosity.js
+++ bot/curiosity.js
@@ -1,33 +1,25 @@
 'use strict'
 
+const rp = require('minimal-request-promise')
 const vbTemplate = require('claudia-bot-builder').viberTemplate
 
-module.exports = function rovers() {
-  return new vbTemplate.Text(`Photos from NASA's rovers on Mars, choose which ones pictures would you like to see`)
-    .addReplyKeyboard(true)
-    .addKeyboardButton(`<b>Curiosity</b>`, 'Curiosity', 6, 2, {
-      TextSize: 'large',
-      BgColor: '#f6d95e',
-      BgMediaType: 'picture',
-      BgMedia: 'https://s3.eu-central-1.amazonaws.com/laptopfriendly/lf-buttons-all-locations-icons.png'
+module.exports = function peopleInSpace() {
+  return rp.get('http://api.open-notify.org/astros.json')
+    .then(response => {
+      const inSpace = JSON.parse(response.body)
+      return [
+        new vbTemplate.Text(`There are ${inSpace.number} people in Space right now.`).get(),
+        new vbTemplate.Text(inSpace.people.reduce((response, person) => {
+          return response + `- ${person.name}` + ((person.craft) ? ` is on ${person.craft}` : '') + ';\n'
+        }))
+          .addReplyKeyboard(true)
+          .addKeyboardButton(`<b>Back to start</b>`, 'Start', 6, 1, {
+            TextSize: 'large',
+            BgColor: '#f6d95e',
+            BgMediaType: 'picture',
+            BgMedia: 'https://s3.eu-central-1.amazonaws.com/laptopfriendly/lf-buttons-all-locations-icons.png'
+          })
+          .get()
+      ]
     })
-    .addKeyboardButton(`<b>Opportunity</b>`, 'Opportunity', 6, 2, {
-      TextSize: 'large',
-      BgColor: '#f6d95e',
-      BgMediaType: 'picture',
-      BgMedia: 'https://s3.eu-central-1.amazonaws.com/laptopfriendly/lf-buttons-all-locations-icons.png'
-    })
-    .addKeyboardButton(`<b>Spirit</b>`, 'Spirit', 6, 2, {
-      TextSize: 'large',
-      BgColor: '#f6d95e',
-      BgMediaType: 'picture',
-      BgMedia: 'https://s3.eu-central-1.amazonaws.com/laptopfriendly/lf-buttons-all-locations-icons.png'
-    })
-    .addKeyboardButton(`<b>Back to start</b>`, 'Start', 6, 1, {
-      TextSize: 'large',
-      BgColor: '#f6d95e',
-      BgMediaType: 'picture',
-      BgMedia: 'https://s3.eu-central-1.amazonaws.com/laptopfriendly/lf-buttons-all-locations-icons.png'
-    })
-    .get()
 }
