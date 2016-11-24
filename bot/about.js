6e4a93e004f88f01112b41705c991acf8a41dbef
simalexan <alexander.simovic@gmail.com>
2016-11-24 02:19:11 +0100

    minor fixes

--- bot/about.js
+++ bot/about.js
@@ -4,7 +4,7 @@ const vbTemplate = require('claudia-bot-builder').viberTemplate
 
 module.exports = function about() {
   return [
-    new vbTemplate.Text(`Space Explorer is simple Messenger chat bot that uses NASA's API to get the data and images about the space`).get(),
+    new vbTemplate.Text(`Space Explorer is simple Viber chat bot that uses NASA's API to get the data and images about the space`).get(),
     new vbTemplate.Text(`It's created for fun and also as a showcase for Claudia Bot Builder, a Node.js library for creating chat/voice bots for various platform and deploying them on AWS Lambda`)
       .addReplyKeyboard(true)
       .addKeyboardButton(`<b>Claudia Bot Builder</b>`, 'https://github.com/claudiajs/claudia-bot-builder', 3, 2, {
--- bot/people-in-space.js
+++ bot/people-in-space.js
@@ -7,11 +7,10 @@ module.exports = function peopleInSpace() {
   return rp.get('http://api.open-notify.org/astros.json')
     .then(response => {
       const inSpace = JSON.parse(response.body)
-      console.log(inSpace)
       return [
         new vbTemplate.Text(`There are ${inSpace.number} people in Space right now.`).get(),
         new vbTemplate.Text(inSpace.people.reduce((response, person) => {
-          return response + `- ${person.name}` + ((person.craft) ? ` is on ${person.craft}` : '') + ';\n'
+          return `- ${person.name}` + ((person.craft) ? ` is on ${person.craft}` : '') + ';\n'
         }))
           .addReplyKeyboard(true)
           .addKeyboardButton(`<b>Back to start</b>`, 'Start', 6, 1, {
