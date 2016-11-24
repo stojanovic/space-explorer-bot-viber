343a824df054b601ed9c13f98637009b7363413f
simalexan <alexander.simovic@gmail.com>
2016-11-24 02:13:10 +0100

    fixing an issue with message text

--- bot/flow.js
+++ bot/flow.js
@@ -17,16 +17,16 @@ module.exports = function spaceFlow (message, request) {
   if (isUrl(message.text)) // Do nothing if user sends an url, because it's a reply to an URL button
     return true
 
-  if (request.text == 'Start') return startMenu()
-  if (request.text == 'APOD') return apod(request)
-  if (request.text == 'What is a Picture of the Day') return whatIsApod()
-  if (request.text == 'Mars Rovers') return rovers()
-  if (request.text == 'ISS') return iss(request)
-  if (request.text == 'People in Space') return peopleInSpace()
-  if (request.text == 'About') return about()
-  if (request.text == 'Curiosity') return curiosity()
-  if (request.text == 'Opportunity') return opportunity()
-  if (request.text == 'Spirit') return spirit()
+  if (message.text == 'Start') return startMenu()
+  if (message.text == 'APOD') return apod(request)
+  if (message.text == 'What is a Picture of the Day') return whatIsApod()
+  if (message.text == 'Mars Rovers') return rovers()
+  if (message.text == 'ISS') return iss(request)
+  if (message.text == 'People in Space') return peopleInSpace()
+  if (message.text == 'About') return about()
+  if (message.text == 'Curiosity') return curiosity()
+  if (message.text == 'Opportunity') return opportunity()
+  if (message.text == 'Spirit') return spirit()
   
   return startMenu()
 }
