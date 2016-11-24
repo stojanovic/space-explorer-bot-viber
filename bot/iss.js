a0a7d7dd0bb88da12343b2a462c33ac2b52605e2
simalexan <alexander.simovic@gmail.com>
2016-11-24 02:39:23 +0100

    fixed ISS position

--- bot/iss.js
+++ bot/iss.js
@@ -7,10 +7,10 @@ module.exports = function iss() {
   return rp.get('https://api.wheretheiss.at/v1/satellites/25544')
     .then(response => {
       const ISS = JSON.parse(response.body)
-      console.log(ISS)
       return [
+        new vbTemplate.Text(`International Space station is currently at `).get(),
         new vbTemplate.Location(ISS.latitude, ISS.longitude).get(),
-        new vbTemplate.Text(`International Space station`)
+        new vbTemplate.Text(`You can zoom out a bit to get a better ISS position overview`)
           .addReplyKeyboard(true)
           .addKeyboardButton(`<b>Website</b>`, 'http://iss.astroviewer.net', 6, 2, {
             TextSize: 'large',
