44375d5c97cc82373dd9cf36ea2e4030ae9fbd29
simalexan <alexander.simovic@gmail.com>
2016-11-24 02:46:16 +0100

    fixing the font color of the start menu

--- bot/start-menu.js
+++ bot/start-menu.js
@@ -5,25 +5,25 @@ const vbTemplate = require('claudia-bot-builder').viberTemplate
 module.exports = function startMenu(text) {
   return new vbTemplate.Text(text || `Hello this is Space Explorer, what would you like me to do:`)
     .addReplyKeyboard(true)
-      .addKeyboardButton(`<b>Astronomy Picture of the Day</b>`, 'APOD', 3, 2, {
+      .addKeyboardButton(`<font color="#FFFFFF"><b>Astronomy Picture of the Day</b></font>`, 'APOD', 3, 2, {
         TextSize: 'large',
         BgColor: '#f6d95e',
         BgMediaType: 'picture',
         BgMedia: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot-viber/master/images/galaxy.jpeg'
       })
-      .addKeyboardButton(`<b>Photos from Rovers on Mars</b>`, 'Mars Rovers', 3, 2, {
+      .addKeyboardButton(`<font color="#FFFFFF"><b>Photos from Rovers on Mars</b></font>`, 'Mars Rovers', 3, 2, {
         TextSize: 'large',
         BgColor: '#f6d95e',
         BgMediaType: 'picture',
         BgMedia: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot-viber/master/images/curiosity.jpg'
       })
-      .addKeyboardButton(`<b>Where is the ISS now?</b>`, 'ISS', 3, 2, {
+      .addKeyboardButton(`<font color="#FFFFFF"><b>Where is the ISS now?</b></font>`, 'ISS', 3, 2, {
         TextSize: 'large',
         BgColor: '#f6d95e',
         BgMediaType: 'picture',
         BgMedia: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot-viber/master/images/iss.jpg'
       })
-      .addKeyboardButton(`<b>How many people are in space right now?</b>`, 'People in Space', 3, 2, {
+      .addKeyboardButton(`<font color="#FFFFFF"><b>How many people are in space right now?</b></font>`, 'People in Space', 3, 2, {
         TextSize: 'large',
         BgColor: '#f6d95e',
         BgMediaType: 'picture',
