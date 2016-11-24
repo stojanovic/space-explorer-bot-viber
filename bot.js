ba77a274bb73aea8d16568303c1edfc4cc3cd267
simalexan <alexander.simovic@gmail.com>
2016-11-24 02:24:58 +0100

    adding logs and location.get

--- bot.js
+++ bot.js
@@ -5,5 +5,5 @@ const spaceFlow = require('./bot/flow')
 
 module.exports = botBuilder((message, req) => {
   console.log(message, req)
-  return spaceFlow(message, req) //`Hello this is Space Explorer, your message was ${request.text}.`;
+  return spaceFlow(message, req)
 });
--- bot/iss.js
+++ bot/iss.js
@@ -4,12 +4,14 @@ const rp = require('minimal-request-promise')
 const vbTemplate = require('claudia-bot-builder').viberTemplate
 
 module.exports = function iss(request) {
+  console.log(request.env)
+  console.log(request.env.nasaApiKey)
   return rp.get(`https://api.nasa.gov/planetary/apod?api_key=${request.env.nasaApiKey}`)
     .then(response => {
       const ISS = JSON.parse(response.body)
-
+      console.log(ISS)
       return [
-        new vbTemplate.Location(ISS.latitude, ISS.longitude),
+        new vbTemplate.Location(ISS.latitude, ISS.longitude).get(),
         new vbTemplate.Text(`International Space station`)
           .addReplyKeyboard(true)
           .addKeyboardButton(`<b>Website</b>`, 'http://iss.astroviewer.net', 6, 2, {
