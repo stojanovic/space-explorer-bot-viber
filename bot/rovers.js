5aabad2bd569a68e1dd30ebed36b3be801746122
simalexan <alexander.simovic@gmail.com>
2016-11-24 03:45:46 +0100

    adding rovers white font color

--- bot/rovers.js
+++ bot/rovers.js
@@ -5,19 +5,19 @@ const vbTemplate = require('claudia-bot-builder').viberTemplate
 module.exports = function rovers() {
   return new vbTemplate.Text(`Photos from NASA's rovers on Mars, choose which ones pictures would you like to see`)
       .addReplyKeyboard(true)
-      .addKeyboardButton(`<b>Curiosity</b>`, 'Curiosity', 6, 2, {
+      .addKeyboardButton(`<font color="#FFFFFF"><b>Curiosity</b></font>`, 'Curiosity', 6, 2, {
         TextSize: 'large',
         BgColor: '#f6d95e',
         BgMediaType: 'picture',
         BgMedia: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot-viber/master/images/curiosity-rover.jpg'
       })
-      .addKeyboardButton(`<b>Opportunity</b>`, 'Opportunity', 6, 2, {
+      .addKeyboardButton(`<font color="#FFFFFF"><b>Opportunity</b></font>`, 'Opportunity', 6, 2, {
         TextSize: 'large',
         BgColor: '#f6d95e',
         BgMediaType: 'picture',
         BgMedia: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot-viber/master/images/opportunity-rover.jpeg'
       })
-      .addKeyboardButton(`<b>Spirit</b>`, 'Spirit', 6, 2, {
+      .addKeyboardButton(`<font color="#FFFFFF"><b>Spirit</b></font>`, 'Spirit', 6, 2, {
         TextSize: 'large',
         BgColor: '#f6d95e',
         BgMediaType: 'picture',
